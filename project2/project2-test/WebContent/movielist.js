c72f428e02ec0e432e4a5892e930bb9908485572
Yuxiang Wu <yuxianw5@uci.edu>
2018-05-03 12:53:11 -0700

    movie list needs to be modified in js file

---
+++ project2/project2-test/WebContent/movielist.js
@@ -0,0 +1,96 @@
+/**
+ * The movielist.js needs to handle the normal search submit event
+ * which happened on the head of page
+ * 
+ * However, its main job is getting the parameter from url, sent it to the MovieListServlet
+ * and show the result accroding to the response
+ */
+
+function handleSearchResult(resultData) {
+    console.log("handleMainSearchResult: jump to the corresponding movie list page");
+    window.location.replace("movie-list.html?title="+resultData[0]["movie_title"]);    
+}
+function submitMainSearch(formSubmitEvent) {
+	console.log("submit main search in movie-list page");
+    console.log(jQuery("#main_search").serialize());
+    // Important: disable the default action of submitting the form
+    //   which will cause the page to refresh
+    //   see jQuery reference for details: https://api.jquery.com/submit/
+    formSubmitEvent.preventDefault();
+    
+    jQuery.ajax({
+        dataType: "json", // Setting return data type
+        method: "GET", // Setting request method
+        url: "api/main", // Setting request url, which is mapped to the TestServlet
+        data: jQuery("#main_search").serialize(),
+        success: (resultData) => handleSearchResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+    });
+}
+//Bind the submit action of the form to a handler function
+jQuery("#main_search").submit((event) => submitMainSearch(event));
+
+
+/**
+ * Once this .js is loaded, following scripts will be executed by the browser
+ */
+function getParameterByName(target) {
+    // Get request URL
+    let url = window.location.href;
+    console.log("url: "+url);
+    // Encode target parameter name to url encoding
+    target = target.replace(/[\[\]]/g, "\\$&");
+    
+
+    // Use regular expression to find matched parameter value
+    let regex = new RegExp("[?&]" + target + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    results[2]=results[2].split("%").join("");
+    // Return the decoded parameter value
+    console.log("result: "+ decodeURIComponent(results[2].replace(/\+/g, " ")));
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function handleMovieResult(resultData) {
+
+    console.log("handleResult: populating movie info from resultData");
+
+    // populate the star info h3
+    // find the empty h3 body by id "star_info"
+    /*let starInfoElement = jQuery("#star_info");
+
+    // append two html <p> created to the h3 body, which will refresh the page
+    starInfoElement.append("<p>Star Name: " + resultData[0]["star_name"] + "</p>" +
+        "<p>Date Of Birth: " + resultData[0]["star_dob"] + "</p>");
+    */
+
+    console.log("handleResult: populating movie table from resultData");
+
+    // Populate the star table
+    // Find the empty table body by id "movie_table_body"
+    let movieTableBodyElement = jQuery("#movie_table_body");
+
+    // Concatenate the html tags with resultData jsonObject to create table rows
+    for (let i = 0; i < resultData.length; i++) {
+        let rowHTML = "";
+        rowHTML += "<tr>";
+        rowHTML += "<th>" + resultData[i]["movie_title"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_year"] + "</th>";
+        rowHTML += "<th>" + resultData[i]["movie_director"] + "</th>";
+        rowHTML += "</tr>";
+
+        // Append the row created to the table body, which will refresh the page
+        movieTableBodyElement.append(rowHTML);
+    }
+}
+
+
+let movieTitle = getParameterByName('title');
+// Makes the HTTP GET request and registers on success callback function handleStarResult
+jQuery.ajax({
+    dataType: "json", // Setting return data type
+    method: "GET", // Setting request method
+    url: "api/movielist?title="+movieTitle, // Setting request url, which is mapped by StarsServlet in Stars.java
+    success: (resultData) => handleMovieResult(resultData) // Setting callback function to handle data returned successfully by the StarsServlet
+});
--- project2/project2-test/src/TestServlet.java
+++ project2/project2-test/src/TestServlet.java
@@ -1,18 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
+import javax.annotation.Resource;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
+import javax.sql.DataSource;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet(name = "TestServlet", urlPatterns = "/session/*")
+@WebServlet(name = "TestServlet", urlPatterns = "/api/main")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	@Resource(name = "jdbc/moviedb")
+	private DataSource dataSource;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -27,63 +39,21 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html");
+		response.setContentType("application/json");
         PrintWriter out = response.getWriter();
-        String title = "Session Tracking Example";
-        HttpSession session = request.getSession(true); // Get a instance of current session on the request
-        String heading;
-        Integer accessCount = (Integer) session.getAttribute("accessCount"); // Retrieve data named "accessCount" from session, which count how many times the user requested before
-
-        if (accessCount == null) {
-            // Which means the user is never seen before
-            accessCount = 0;
-            heading = "Welcome, New-Comer";
-        } else {
-            // Which means the user has requested before, thus user information can be found in the session
-            heading = "Welcome Back";
-            accessCount++;
-        }
-
-        // Update the new accessCount to session, replacing the old value if existed
-        session.setAttribute("accessCount", accessCount);
-
-        out.println("<html><head><title>" + title + "</title></head>\n" +  // Create title of the page
-
-                "<body bgcolor=\"#FDF5E6\">\n" +
-                "<h1 ALIGN=\"center\">" + heading + "</h1>\n" +  // Set the greeting heading generated before
-                "<h2>Information on Your Session:</H2>\n" +
-                // Create a <table>
-                "<table border=1 align=\"center\">\n" +
-                "  <tr bgcolor=\"#FFAD00\">\n" +  // Create a <tr> (table row)
-                "    <th>Info Type<th>Value\n" +  // Create two <th>s (table header)
-
-                // Create a <tr> (table row)
-                "  <tr>\n" +
-                "    <td>ID\n" +  // Create the first <td> (table data) in <tr>, which corresponding to the first column
-                "    <td>" + session.getId() + "\n" + // Create the second <td> (table data) in <tr>, which corresponding to the second column
-
-                // Repeat for more table rows and data.
-                "  <tr>\n" +
-                "    <td>Creation Time\n" +
-                "    <td>" +
-                new Date(session.getCreationTime()) + "\n" +
-                "  <tr>\n" +
-                "    <td>Time of Last Access\n" +
-                "    <td>" +
-                new Date(session.getLastAccessedTime()) + "\n" +
-                "  <tr>\n" +
-                "    <td>Number of Previous Accesses\n" +
-                "    <td>" + accessCount + "\n" +
-                "  </tr>" +
-                "</table>\n");
-
-        // The following two statements show how to retrieve parameters in the request.  The URL format is something like:
-        // http://localhost:8080/project2-session-example/Session?myname=Chen%20Li
-        String myname = request.getParameter("myname");
-        if (myname != null)
-            out.println("Hey " + myname + "<br><br>");
-
-        out.println("</body></html>");
+        String title = request.getParameter("title");
+        //add the wildcard
+        title=title.concat("%");
+        JsonArray jsonArray = new JsonArray();
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("movie_title", title);
+        jsonArray.add(jsonObject);
+     // write JSON string to output
+        out.write(jsonArray.toString());
+        // set response status to 200 (OK)
+        response.setStatus(200);
+        System.out.println("Title: "+title);
+        out.close();
     }
 	
 
